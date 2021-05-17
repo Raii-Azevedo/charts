854bdb47d40ff4ed711e1d0611db3181d53fd776
Raissa Azevedo <rhaii.azevedo@gmail.com>
2021-05-17 12:23:55 -0300

    Update settings.py

--- charts/settings.py
+++ charts/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x2l5m&#=7e+xv&ixz$=q@b+3l!!msu0(=*1gzh8m3$4a#5u#&='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
